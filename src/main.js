688cf7faff61af5f3f0bfe1181f0393265b57e1a
root <root@DESKTOP-S90TR06.localdomain>
2020-07-24 08:06:19 -0600

    Fixed parserMiddleware export

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import parse from './methods/parse.js';
-import parseMiddleware from './parseMiddleware.js';
+import parseMiddleware from './middlewares/parseMiddleware.js';
 // import buildMiddleware from './buildMiddleware.js';
 import build from './methods/build.js';
 
